f274d0a455a3ca17176d421dab6b3c1fe81bd192
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2022-05-25 12:06:25 +0200

    chore(scriopt): add repository script

---
+++ scripts/new_repository.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Repository declaration
+echo "---
+apiVersion: source.toolkit.fluxcd.io/v1beta1
+kind: HelmRepository
+metadata:
+  name: $1
+  namespace: flux-system
+spec:
+  interval: 30m
+  url: $2
+  timeout: 3m" > cluster/base/flux-system/charts/helm/$1.yaml
+
+# Include new repository in cluster
+yq -i ".resources += \"$1.yaml\"" cluster/base/flux-system/charts/helm/kustomization.yaml
+
+# Create Namespace folder
+mkdir -p cluster/apps/$1
+
+# Include new namespace
+yq -i ".resources += \"$1\"" cluster/apps/kustomization.yaml
+
+# Basic kustomization file
+echo "---
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+" > cluster/apps/$1/kustomization.yaml
