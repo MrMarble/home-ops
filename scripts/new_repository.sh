bb3b8f850ebea95a576c7e99662f2db1811a133e
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2022-05-25 12:08:21 +0200

    chore(scriopt): add repository script

--- scripts/new_repository.sh
+++ scripts/new_repository.sh
@@ -14,16 +14,3 @@ spec:
 
 # Include new repository in cluster
 yq -i ".resources += \"$1.yaml\"" cluster/base/flux-system/charts/helm/kustomization.yaml
-
-# Create Namespace folder
-mkdir -p cluster/apps/$1
-
-# Include new namespace
-yq -i ".resources += \"$1\"" cluster/apps/kustomization.yaml
-
-# Basic kustomization file
-echo "---
-apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-resources:
-" > cluster/apps/$1/kustomization.yaml
