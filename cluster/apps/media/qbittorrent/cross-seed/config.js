dc11858a598f5c04b3709410898c3465e3808c38
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-02-01 21:41:58 +0100

    feat(qbittorrent): deploy cross-seed

---
+++ cluster/apps/media/qbittorrent/cross-seed/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  delay: 10,
+  torznab: [],
+  torrentDir: "/torrents/qBittorrent/BT_backup",
+  outputDir: "/cross-seeds",
+  includeEpisodes: false,
+  includeNonVideos: false,
+  fuzzySizeThreshold: 0.2,
+  excludeOlder: undefined,
+  excludeRecentSearch: undefined,
+  action: "inject",
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local",
+  duplicateCategories: false,
+  rssCadence: undefined,
+  searchCadence: undefined,
+  notificationWebhookUrl: undefined
+}
