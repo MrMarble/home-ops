f3b6f7bc22dfbb24eeba85cc22403527b57862c0
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-08-08 13:18:07 +0200

    feat: set nodes as control plane

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -3,3 +3,4 @@
 
 talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-kmaster1.yaml
 talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-kworker1.yaml
+talosctl apply-config -n 192.168.1.12 -f ./clusterconfig/home-cluster-kworker2.yaml
