b9ae48d1952130c166c2da17d8dff87b51a3dc93
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-08-20 12:08:04 +0200

    refactor: rename nodes

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-kmaster1.yaml
-talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-kworker1.yaml
-talosctl apply-config -n 192.168.1.12 -f ./clusterconfig/home-cluster-kworker2.yaml
+talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-caos.yaml
+talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-gaia.yaml
+talosctl apply-config -n 192.168.1.12 -f ./clusterconfig/home-cluster-tartaro.yaml
