5871c819d3b5bd378c2baad7bc8cca5340816176
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-11-15 18:25:10 +0100

    feat: add new node

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -4,3 +4,4 @@
 talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-caos.yaml
 talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-gaia.yaml
 talosctl apply-config -n 192.168.1.12 -f ./clusterconfig/home-cluster-tartaro.yaml
+talosctl apply-config -n 192.168.1.13 -f ./clusterconfig/home-cluster-erebos.yaml
