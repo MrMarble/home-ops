0d97595f6d5baf955d25cd2e017d26251bf9c897
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-12-17 16:16:21 +0100

    feat: remove tartaro from cluster

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -3,6 +3,5 @@
 
 talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-caos.yaml
 talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-gaia.yaml
-talosctl apply-config -n 192.168.1.12 -f ./clusterconfig/home-cluster-tartaro.yaml
 talosctl apply-config -n 192.168.1.13 -f ./clusterconfig/home-cluster-erebos.yaml
 talosctl apply-config -n 192.168.1.14 -f ./clusterconfig/home-cluster-cronos.yaml
