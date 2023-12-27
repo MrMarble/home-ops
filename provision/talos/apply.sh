4dd03e45cabcfae7c2d11f93ed51fd39aa39f564
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-12-27 13:42:33 +0100

    feat: deploy mayastor

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/home-cluster-caos.yaml
-talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/home-cluster-gaia.yaml
-talosctl apply-config -n 192.168.1.13 -f ./clusterconfig/home-cluster-erebos.yaml
-talosctl apply-config -n 192.168.1.14 -f ./clusterconfig/home-cluster-cronos.yaml
+talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/k8s-caos.yaml
+talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/k8s-gea.yaml
+talosctl apply-config -n 192.168.1.14 -f ./clusterconfig/k8s-cronos.yaml
