ddcf52737ebda632a15d49ae44b027f5f3a09bcc
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-12-29 15:59:49 +0100

    feat: configure worker node

--- provision/talos/apply.sh
+++ provision/talos/apply.sh
@@ -4,3 +4,4 @@
 talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/k8s-caos.yaml
 talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/k8s-gea.yaml
 talosctl apply-config -n 192.168.1.14 -f ./clusterconfig/k8s-cronos.yaml
+talosctl apply-config -n 192.168.1.13 -f ./clusterconfig/k8s-eros.yaml
