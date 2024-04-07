710197d1924b414ba950c5fd20e171bc1a7fd1ae
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2024-04-07 23:18:05 +0200

    feat: move to ansible

--- provision/talos/apply.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-talosctl apply-config -n 192.168.1.10 -f ./clusterconfig/k8s-caos.yaml
-talosctl apply-config -n 192.168.1.11 -f ./clusterconfig/k8s-gea.yaml
-talosctl apply-config -n 192.168.1.14 -f ./clusterconfig/k8s-cronos.yaml
-talosctl apply-config -n 192.168.1.13 -f ./clusterconfig/k8s-eros.yaml
